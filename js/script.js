4d2732e3164f5ac66b7aca65830da30ddd9897e3
Etzelaire <etzelaire@gmail.com>
2022-07-11 21:38:33 +0200

    d

--- js/script.js
+++ js/script.js
@@ -324,3 +324,26 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+// Creating and inserting elements
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.textContent =
+  "We use cookies for improved functionality and analytics.";
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    // message.remove();
+    message.parentElement.removeChild(message);
+  });
