22fc3bf2f6751747a307ac01f77e3c07bdecbf62
Etzelaire <etzelaire@gmail.com>
2022-07-25 22:31:24 +0200

    xc

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ const btnScrollTo = document.querySelector(".btn--scroll-to");
 const btnScrollTo1 = document.querySelector(".btn--scroll-to-1");
 const section3 = document.querySelector("#section3");
 const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 console.log("A page produced and edited by Etzelaire");
 
@@ -229,144 +232,147 @@ checkFlexGap();
 //BACK TO TOP BUTTON//
 
 //////////////////////////////////////
+///////////////////////////////////////
 // Slider
-// const slider = function () {
-//   const slides = document.querySelectorAll(".slide");
-//   const btnLeft = document.querySelector(".slider__btn--left");
-//   const btnRight = document.querySelector(".slider__btn--right");
-//   const dotContainer = document.querySelector(".dots");
+const slider = function () {
+  const slides = document.querySelectorAll(".slide");
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+  const dotContainer = document.querySelector(".dots");
 
-//   let curSlide = 0;
-//   const maxSlide = slides.length;
+  let curSlide = 0;
+  const maxSlide = slides.length;
 
-//   // Functions
-//   const createDots = function () {
-//     slides.forEach(function (_, i) {
-//       dotContainer.insertAdjacentHTML(
-//         "beforeend",
-//         `<button class="dots__dot" data-slide="${i}"></button>`
-//       );
-//     });
-//   };
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
 
-//   const activateDot = function (slide) {
-//     document
-//       .querySelectorAll(".dots__dot")
-//       .forEach((dot) => dot.classList.remove("dots__dot--active"));
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
 
-//     document
-//       .querySelector(`.dots__dot[data-slide="${slide}"]`)
-//       .classList.add("dots__dot--active");
-//   };
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
 
-//   const goToSlide = function (slide) {
-//     slides.forEach(
-//       (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-//     );
-//   };
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
 
-// Next slide
-// const nextSlide = function () {
-//   if (curSlide === maxSlide - 1) {
-//     curSlide = 0;
-//   } else {
-//     curSlide++;
-//   }
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
 
-//   goToSlide(curSlide);
-//   activateDot(curSlide);
-// };
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
 
-// const prevSlide = function () {
-//   if (curSlide === 0) {
-//     curSlide = maxSlide - 1;
-//   } else {
-//     curSlide--;
-//   }
-//   goToSlide(curSlide);
-//   activateDot(curSlide);
-// };
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
 
-// const init = function () {
-//   goToSlide(0);
-//   createDots();
+  const init = function () {
+    goToSlide(0);
+    createDots();
 
-//   activateDot(0);
-// };
-// init();
+    activateDot(0);
+  };
+  init();
+
+  // Event handlers
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider();
+
 // Lazy loading images
-// const imgTargets = document.querySelectorAll("img[data-src]");
+const imgTargets = document.querySelectorAll("img[data-src]");
 
-// const loadImg = function (entries, observer) {
-//   const [entry] = entries;
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
 
-//   if (!entry.isIntersecting) return;
+  if (!entry.isIntersecting) return;
 
-// Replace src with data-src
-//   entry.target.src = entry.target.dataset.src;
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
 
-//   entry.target.addEventListener("load", function () {
-//     entry.target.classList.remove("lazy-img");
-//   });
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
 
-//   observer.unobserve(entry.target);
-// };
+  observer.unobserve(entry.target);
+};
 
-// const imgObserver = new IntersectionObserver(loadImg, {
-//   root: null,
-//   threshold: 0,
-//   rootMargin: "200px",
-// });
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
 
-// imgTargets.forEach((img) => imgObserver.observe(img));
-// // Event handlers
-// btnRight.addEventListener("click", nextSlide);
-// btnLeft.addEventListener("click", prevSlide);
-
-// document.addEventListener("keydown", function (e) {
-//   if (e.key === "ArrowLeft") prevSlide();
-//   e.key === "ArrowRight" && nextSlide();
-// });
-
-// dotContainer.addEventListener("click", function (e) {
-//   if (e.target.classList.contains("dots__dot")) {
-//     const { slide } = e.target.dataset;
-//     goToSlide(slide);
-//     activateDot(slide);
-//   }
-// });
-
-// slider();
+imgTargets.forEach((img) => imgObserver.observe(img));
 
 // Tabbed component
-// tabsContainer.addEventListener("click", function (e) {
-//   const clicked = e.target.closest(".operations__tab");
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
 
-//   // Guard clause
-//   if (!clicked) return;
+  // Guard clause
+  if (!clicked) return;
 
-//   // Remove active classes
-//   tabs.forEach((t) => t.classList.remove("operations__tab--active"));
-//   tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+  // Remove active classes
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
 
-//   // Activate tab
-//   clicked.classList.add("operations__tab--active");
+  // Activate tab
+  clicked.classList.add("operations__tab--active");
 
-//   // Activate content area
-//   document
-//     .querySelector(`.operations__content--${clicked.dataset.tab}`)
-//     .classList.add("operations__content--active");
-// });
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
 
-// document.querySelector(".nav__links").addEventListener("click", function (e) {
-//   e.preventDefault();
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
 
-//   // Matching strategy
-//   if (e.target.classList.contains("nav__link")) {
-//     const id = e.target.getAttribute("href");
-//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-//   }
-// });
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
 
 ///////////////////////////////////////
 // Page navigation
@@ -401,7 +407,6 @@ function makeSmall() {
 function makeNormal() {
   myVideo.width = 420;
 }
-
 /////////////////////////////////////////
 //////////////////////////////////////////////
-//drag and drop//
+//PAYMENT HTML//
