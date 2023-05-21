a0ebcc2f4b7981ed7696b1c5709ef22f04986ebf
Etzelaire <etzelaire@gmail.com>
2023-05-21 06:45:28 +0200

    ddsfsfsdf

--- js/script.js
+++ js/script.js
@@ -61,6 +61,29 @@ btnScrollTo1.addEventListener("click", function (e) {
   section1.scrollIntoView({ behavior: "smooth" });
 });
 //////////////////////////
+document.addEventListener('DOMContentLoaded', (event) => {
+  //Get the button
+  var mybutton = document.getElementById("back-to-top-btn");
+
+  // When the user scrolls down from the height of the window, show the button
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+      if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
+          mybutton.style.display = "block";
+      } else {
+          mybutton.style.display = "none";
+      }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+      window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+
+  // attach the click event to the button here, inside DOMContentLoaded event listener
+  mybutton.addEventListener("click", topFunction);
+});
 
 ///////////////////////////////////////////////////////////
 // PAGE NAVIGATION
@@ -184,3 +207,35 @@ imgTargets.forEach((img) => imgObserver.observe(img));
 //   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 // }
 // checkFlexGap();
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Get the hamburger button, close button and the nav menu
+  var hamburger = document.querySelector(".icon-mobile-nav[name='menu-outline']");
+  var close = document.querySelector(".icon-mobile-nav[name='close-outline']");
+  var nav = document.querySelector(".main-nav");
+
+  // Attach click event to the hamburger button
+  hamburger.addEventListener("click", function() {
+      hamburger.style.display = "none";
+      close.style.display = "block";
+      nav.style.display = "block";
+  });
+
+  // Attach click event to the close button
+  close.addEventListener("click", function() {
+      close.style.display = "none";
+      hamburger.style.display = "block";
+      nav.style.display = "none";
+  });
+
+  // Listen for resize events
+  window.addEventListener('resize', function() {
+      // If the screen size is 768px or larger
+      if (window.innerWidth >= 768) {
+          // Reset everything to the default state
+          hamburger.style.display = "block";
+          close.style.display = "none";
+          nav.style.display = "none";
+      }
+  });
+});
+
